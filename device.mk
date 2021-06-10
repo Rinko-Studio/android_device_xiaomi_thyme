77e9181cb3306b12cb74ca2f5b17b8818f025816
micky387 <mickaelsaibi@free.fr>
2021-06-10 19:46:35 +0200

    thyme: Import and build bootctrl from CAF
    This is a squash of:
    
    1.
    Author:    micky387 <mickaelsaibi@free.fr>
    Date:      Thu Jun 10 19:46:35 2021 +0200
    
        Import and build bootctrl from CAF
        Bootctrl: https://source.codeaurora.org/quic/la/platform/hardware/qcom/bootctrl/tree/?h=LA.UM.9.14.r1-16700-LAHAINA.0
    
        recovery-ufs-bsg: https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/recovery-ext/tag/?h=LA.UM.9.14.r1-16700-LAHAINA.0
    
        Recovery:
          * Removed all guard related to the ufs frameworks. Lahaina use it so ne need to add additional stuff for it (soong namespace)
           -https://source.codeaurora.org/quic/la/platform/vendor/qcom/lahaina/commit/BoardConfig.mk?h=LA.UM.9.14.r1-16300-LAHAINA.0&id=ffb36a29fbac3a75130ea6e0cc159958bf55dab6
          * Removed too, the guard related to GENERIC_KERNEL_HEADERS. Our kernel (5.4) doesn't support previous scsi ufs
    
    2.
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Thu May 27 20:44:42 2021 +0200
    
        gpt-utils: Fix build errors
    
        Change-Id: Ie8245d1637e12b5979a8af1b864cc713135b5c95
    
    Change-Id: Id8eac3c6fc312ee1e2ac5b83275ed7b2bb62ec10

--- device.mk
+++ device.mk
@@ -34,6 +34,15 @@ AB_OTA_POSTINSTALL_CONFIG += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    bootctrl.kona.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
@@ -42,7 +51,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
-
+kona
 # F2FS utilities
 PRODUCT_PACKAGES += \
     sg_write_buffer \

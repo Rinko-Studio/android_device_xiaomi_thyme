c887f7a5e6d8bceecf570a4297170934ba56f94e
kubersharma001 <kubersharma001@gmail.com>
2021-10-21 15:41:57 +0530

    thyme: Initial props import
    Change-Id: I14ead6b4b25c10b5fa776779c78cafd9197a4b55

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,6 +108,10 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := kona
 
 # Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/properties/system_ext.prop
+
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 
 # Releasetools

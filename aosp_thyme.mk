1979dd8cc0a1dea1f70717cabf331dd2a787289a
LynnrinChan <wuxiaofei31@gmail.com>
2022-04-13 09:44:51 +0000

    thyme: PE-ify

--- aosp_thyme.mk
+++ aosp_thyme.mk
@@ -11,8 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/thyme/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common PixelExperience configurations
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
@@ -20,6 +24,6 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := thyme
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2102J2SC
-PRODUCT_NAME := arrow_thyme
+PRODUCT_NAME := aosp_thyme
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

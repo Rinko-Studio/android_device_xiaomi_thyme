125d7bdd2d01b2e7ba8160dfd41436e7b265efc1
CureWhale <97373469+CureWhale@users.noreply.github.com>
2022-05-31 08:42:56 +0800

    Update and rename evolution_thyme.mk to exthm_thyme.mk

--- exthm_thyme.mk
+++ exthm_thyme.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/thyme/device.mk)
 
 # Inherit common ArrowOS configurations
-$(call inherit-product, vendor/evolution/config/common.mk)
+$(call inherit-product, vendor/exthm/config/common.mk)
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
@@ -20,6 +20,6 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := thyme
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2102J2SC
-PRODUCT_NAME := evolution_thyme
+PRODUCT_NAME := exthm_thyme
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
